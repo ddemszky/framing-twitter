dc9e2aa306665b6b17b0cbfd271c3f831a735e6a
Dora <ddemszky@stanford.edu>
2019-02-26 18:55:47 -0500

    created scripts

---
+++ data_exploration/glove_neighbors.py
@@ -0,0 +1,67 @@
+import scipy.spatial.distance
+import pandas as pd
+import numpy as np
+
+# based on a script from Chris Potts' class, CS224U
+
+def cosine(u, v):
+    return scipy.spatial.distance.cosine(u, v)
+def neighbors_word(word, df, distfunc=cosine):
+    """Tool for finding the nearest neighbors of `word` in `df` according
+    to `distfunc`. The comparisons are between row vectors.
+
+    Parameters
+    ----------
+    word : str
+        The anchor word. Assumed to be in `rownames`.
+    df : pd.DataFrame
+        The vector-space model.
+    distfunc : function mapping vector pairs to floats (default: `cosine`)
+        The measure of distance between vectors. Can also be `euclidean`,
+        `matching`, `jaccard`, as well as any other distance measure
+        between 1d vectors.
+
+    Raises
+    ------
+    ValueError
+        If word is not in `df.index`.
+
+    Returns
+    -------
+    pd.Series
+        Ordered by closeness to `word`.
+
+    """
+    if word not in df.index:
+        raise ValueError('{} is not in this VSM'.format(word))
+    w = df.loc[word]
+    dists = df.apply(lambda x: distfunc(w, x), axis=1)
+    return dists.sort_values()
+def neighbors_vector(vector, df, distfunc=cosine):
+    """Tool for finding the nearest neighbors of `word` in `df` according
+    to `distfunc`. The comparisons are between row vectors.
+
+    Parameters
+    ----------
+    word : str
+        The anchor word. Assumed to be in `rownames`.
+    df : pd.DataFrame
+        The vector-space model.
+    distfunc : function mapping vector pairs to floats (default: `cosine`)
+        The measure of distance between vectors. Can also be `euclidean`,
+        `matching`, `jaccard`, as well as any other distance measure
+        between 1d vectors.
+
+    Raises
+    ------
+    ValueError
+        If word is not in `df.index`.
+
+    Returns
+    -------
+    pd.Series
+        Ordered by closeness to `word`.
+
+    """
+    dists = df.apply(lambda x: distfunc(vector, x), axis=1)
+    return dists.sort_values()
