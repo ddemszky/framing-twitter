1474bd8e066cab4f6ca4ec0cb775d15929c98f87
Dora <ddemszky@stanford.edu>
2019-03-06 14:43:48 -0800

    refactored topic tweets sampling

--- 2_topic_clustering/4_compute_cluster_means.py
+++ 2_topic_clustering/4_compute_cluster_means.py
@@ -1,31 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import numpy as np
-import sys
 from nltk.cluster import KMeansClusterer
 import nltk
 import json
-
-# this can be set to any number
-NUM_CLUSTERS = 6
-SAMPLE_SIZE = int(sys.argv[1])
 
 config = json.load(open('../config.json', 'r'))
 INPUT_DIR = config['INPUT_DIR']
 OUTPUT_DIR = config['OUTPUT_DIR']
 TWEET_DIR = config['TWEET_DIR']
+NUM_CLUSTERS = config['NUM_CLUSTERS']
 events = open(INPUT_DIR + 'event_names.txt', 'r').read().splitlines()
 
 
 print('loading...')
-# use a joint sample of embeddings from each event to determine the cluster centroids
 
-def get_samples(sample_size):
+# use a joint sample of embeddings from each event to determine the cluster centroids
+def get_samples():
     tweet_embeds = ''
     for event in events:
         embeds = np.load(TWEET_DIR + event + '/' + event + '_embeddings_partisan.npy')
-        N = embeds.shape[0]
-        embeds = embeds[np.random.choice(N, min(N, sample_size), replace=False), :]
+        indices = np.load(TWEET_DIR + event + '/' + event + '_indices_among_embeddings_for_getting_topics.npy')
+        embeds = embeds[indices, :]
         if len(tweet_embeds) == 0:
             tweet_embeds = embeds
         else:
@@ -33,7 +29,7 @@ def get_samples(sample_size):
 
     return tweet_embeds
 
-tweet_embeds = get_samples(SAMPLE_SIZE)
+tweet_embeds = get_samples()
 
 print(tweet_embeds.shape)
 kclusterer = KMeansClusterer(NUM_CLUSTERS, distance=nltk.cluster.util.cosine_distance, repeats=1)
---
+++ 2_topic_clustering/get_sample_for_inducing_topics.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+import numpy as np
+
+config = json.load(open('../config.json', 'r'))
+INPUT_DIR = config['INPUT_DIR']
+OUTPUT_DIR = config['OUTPUT_DIR']
+TWEET_DIR = config['TWEET_DIR']
+SAMPLE_SIZE = config['SAMPLE_SIZE_FOR_TOPICS']
+events = open(INPUT_DIR + 'event_names.txt', 'r').read().splitlines()
+
+def get_samples(SAMPLE_SIZE):
+    for event in events:
+        # number of tweets that have embeddings
+        N = len(np.load(TWEET_DIR + event + '/' + event + '_cleaned_and_partisan_indices.npy'))
+
+        print(event, N)
+        rand = np.random.choice(N, min(N, SAMPLE_SIZE), replace=False)
+
+        np.save(TWEET_DIR + event + '/' + event + '_indices_among_embeddings_for_getting_topics.npy', rand)
+    return
+
+if __name__ == "__main__":
+    get_samples(SAMPLE_SIZE)
