8e3aef778c82e93ea44dec0ab2c3684f2ba7177e
Dora <ddemszky@stanford.edu>
2019-03-12 14:55:56 -0700

    btm outputs

--- 2_topic_clustering/myBTMexample.sh
+++ 2_topic_clustering/myBTMexample.sh
@@ -7,6 +7,7 @@ alpha=`echo "scale=3;50/$K"|bc`
 beta=0.01
 niter=1000
 save_step=501
+W=1669 # vocab size, determined by the number of entries in `joint_vocab_nostop.json`
 
 btm_dir=/Users/ddemszky/BTM/
 input_dir=${btm_dir}myData/
@@ -18,28 +19,24 @@ mkdir -p $output_dir/model
 doc_pt_train=${input_dir}tweets_train.txt
 doc_pt_all=${input_dir}tweets_all.txt
 
-echo "=============== Index Docs ============="
+echo "=============== Index Tweets for Training ============="
 # docs after indexing
 dwid_pt_train=${output_dir}train_tweet_wids.txt
 dwid_pt_all=${output_dir}all_tweet_wids.txt
-# vocabulary file
-vocab_pt=${output_dir}vocab.txt
 
-python indexDocs.py $doc_pt_train $dwid_pt_train $vocab_pt
-python indexDocs.py $doc_pt_all $dwid_pt_all $vocab_pt
+# index training docs
+python btm_word2idx.py $doc_pt_train $dwid_pt_train
+# index all docs
+python btm_word2idx.py $doc_pt_all $dwid_pt_all
 
 ## learning parameters p(z) and p(w|z)
 echo "=============== Topic Learning ============="
-W=`wc -l < $vocab_pt` # vocabulary size
-make -C ../src
-echo "../src/btm est $K $W $alpha $beta $niter $save_step $dwid_pt_train $model_dir"
-../src/btm est $K $W $alpha $beta $niter $save_step $dwid_pt_train $model_dir
+make -C ${btm_dir}src
+echo "${btm_dir}src/btm est $K $W $alpha $beta $niter $save_step $dwid_pt_train $model_dir"
+${btm_dir}src/btm est $K $W $alpha $beta $niter $save_step $dwid_pt_train $model_dir
 
 ## infer p(z|d) for each doc
-echo "================ Infer P(z|d)==============="
-echo "../src/btm inf sum_b $K $dwid_pt_all $model_dir"
-../src/btm inf sum_b $K $dwid_pt_all $model_dir
+echo "================ Infer P(z|d) for All Tweets==============="
 
-## output top words of each topic
-echo "================ Topic Display ============="
-python topicDisplay.py $model_dir $K $vocab_pt
+echo "${btm_dir}src/btm inf sum_b $K $dwid_pt_all $model_dir"
+${btm_dir}src/btm inf sum_b $K $dwid_pt_all $model_dir
