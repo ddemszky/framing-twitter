c75e9938e269da4ccf17757df4b2775ee4968c7f
Dora <ddemszky@stanford.edu>
2019-05-15 16:39:21 -0700

    refactoring

--- 3_polarization_measures/topic_polarization.py
+++ 3_polarization_measures/topic_polarization.py
@@ -1,45 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import division
-
 import json
 import sys
-
 import pandas as pd
 from joblib import Parallel, delayed
-from calculate_polarization import get_values
 sys.path.append('..')
 from helpers.funcs import *
+from calculate_polarization import *
 
 config = json.load(open('../config.json', 'r'))
 INPUT_DIR = config['INPUT_DIR']
 TWEET_DIR = config['TWEET_DIR']
 NUM_CLUSTERS = config['NUM_CLUSTERS']
 events = open(INPUT_DIR + 'event_names.txt', 'r').read().splitlines()
+parser = argparse.ArgumentParser(description='Computes polarization value between two groups of texts.')
+parser.add_argument('-c','--cluster', help='Kind of cluster method to filter with (only if filtering is "clustered"', default='relative')
+parser.add_argument('-l','--leaveout', help='Whether to use leave-out.', action="store_true")
+parser.add_argument('-m','--method', help='Which method to use: posterior, mutual_information or chi_square', default='posterior')
+args = vars(parser.parse_args())
 
-def get_polarization(event, cluster_method = None):
-    '''
 
-    :param event: name of the event (str)
-    :param cluster_method: None, "relative" or "absolute" (see 5_assign_tweets_to_clusters.py); must have relevant files
-    :return: tuple: (true value, random value)
-    '''
-    data = pd.read_csv(TWEET_DIR + event + '/' + event + '.csv', sep='\t', lineterminator='\n',
-                       usecols=['user_id', 'text', 'dem_follows', 'rep_follows'])
-    data = get_cluster_assignments(event, data, cluster_method)
-
-    print(event, len(data))
+def get_polarization(event):
+    data = load_data(event, "clustered", args['cluster'])
+    if args['method'] == 'posterior':
+        default_score = .5
+    else:
+        default_score = 0
 
     topic_polarization = {}
     for i in range(NUM_CLUSTERS):
         print(i)
-        topic_polarization[i] = tuple(get_values(event, data[data['topic'] == i], leaveout=True))
+        topic_polarization[i] = tuple(get_values(e, data[data['topic'] == i], args['method'], args['leaveout'], False, default_score))
 
-    cluster_method = method_name(cluster_method)
-    with open(TWEET_DIR + event + '/' + event + '_topic_polarization' + cluster_method + '_neutral.json', 'w') as f:
+    cluster_method = method_name(args['cluster'])
+    leaveout = '_leaveout' if args['leaveout'] else ''
+    filename = '_topic_polarization_' + args['method'] + '_' + cluster_method + leaveout + '.json'
+    with open(TWEET_DIR + event + '/' + event + filename, 'w') as f:
         f.write(json.dumps(topic_polarization))
 
-
-cluster_method = None if len(sys.argv) < 2 else sys.argv[1]
-
-Parallel(n_jobs=3)(delayed(get_polarization)(e, cluster_method) for e in events)
+for e in events:
+    get_polarization(e)
+#Parallel(n_jobs=3)(delayed(get_polarization)(e, cluster_method) for e in events)
